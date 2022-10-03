56b08dea2ca16b6457484373ddcf730c3dee576b
Jan Jelínek <105634459+janjelinekjr@users.noreply.github.com>
2022-10-03 16:29:53 +0200

    Update app.ts

--- 12 Google maps prj/prj-00-initial-starting-setup/src/app.ts
+++ 12 Google maps prj/prj-00-initial-starting-setup/src/app.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyDjBKMU5iZeBnVSS8bWVt9SRK6LpmJQ_XA";
+const GOOGLE_API_KEY = "";
 
 // declare var google: any;
 
